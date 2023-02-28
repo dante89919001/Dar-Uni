0ef9e62079ac1284b72a4eb90cf4769f8529d7f6
dante89919001 <62095188+dante89919001@users.noreply.github.com>
2023-03-01 00:07:35 +0600

    Revert "add homework 8"
    This reverts commit 4aedc21140cfce46a2d29463d43497773fbf1a2f.

--- homework8/src/components/user/UserInfo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useUserContext } from '../../providers/UserContext';
-
-type Props = {};
-export const UserInfo: React.FC<Props> = () => {
-  const { username } = useUserContext();
-  return <div>{username}</div>;
-};
-
